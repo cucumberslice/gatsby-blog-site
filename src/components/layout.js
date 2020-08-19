c20147c7b2a73cc2b6d276b00d5bce9acdcf082c
cucumberslice <wangdzi@skywalker.fios-router.home>
2020-08-19 16:51:11 -0400

    changed home to articles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid*/
 
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { Link } from "gatsby";
-import { StaticQuery, graphql } from "gatsby";
-import { HelmetDatoCms } from "gatsby-source-datocms";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
+import { HelmetDatoCms } from 'gatsby-source-datocms';
 
-import "../styles/index.sass";
+import '../styles/index.sass';
 
 const TemplateWrapper = ({ children }) => {
   const [showMenu, setShowMenu] = useState(false);
@@ -43,8 +43,8 @@ const TemplateWrapper = ({ children }) => {
           }
         }
       `}
-      render={data => (
-        <div className={`container ${showMenu ? "is-open" : ""}`}>
+      render={(data) => (
+        <div className={`container ${showMenu ? 'is-open' : ''}`}>
           <HelmetDatoCms
             favicon={data.datoCmsSite.faviconMetaTags}
             seo={data.datoCmsHome.seoMetaTags}
@@ -58,12 +58,12 @@ const TemplateWrapper = ({ children }) => {
                 className="sidebar__intro"
                 dangerouslySetInnerHTML={{
                   __html:
-                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
+                    data.datoCmsHome.introTextNode.childMarkdownRemark.html,
                 }}
               />
               <ul className="sidebar__menu">
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/">Articles</Link>
                 </li>
                 <li>
                   <Link to="/about">About</Link>
@@ -77,7 +77,7 @@ const TemplateWrapper = ({ children }) => {
                     target="blank"
                     className={`social social--${profile.profileType.toLowerCase()}`}
                   >
-                    {" "}
+                    {' '}
                   </a>
                 ))}
               </p>
@@ -91,7 +91,7 @@ const TemplateWrapper = ({ children }) => {
               <div className="mobile-header">
                 <div className="mobile-header__menu">
                   <button
-                    onClick={e => {
+                    onClick={(e) => {
                       e.preventDefault();
                       setShowMenu(!showMenu);
                     }}
@@ -111,7 +111,7 @@ const TemplateWrapper = ({ children }) => {
 };
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.object
+  children: PropTypes.object,
 };
 
 export default TemplateWrapper;
